b28959a41c70af813bd11a31d39eadff981a116a
ksutikm <tsksusha@yandex.ru>
2020-05-05 15:46:07 +0300

    Fixes menu

--- Workvesion/app/src/main/java/com/example/workvesion/NavigationLeft.java
+++ Workvesion/app/src/main/java/com/example/workvesion/NavigationLeft.java
@@ -11,6 +11,7 @@ import androidx.fragment.app.FragmentTransaction;
 
 import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Toast;
@@ -61,6 +62,17 @@ public class NavigationLeft extends AppCompatActivity implements NavigationView.
 
     @Override
     public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+        int size_menu = navigationView.getMenu().size();
+        for (int i = 0; i < size_menu; i++) {
+            final MenuItem items = navigationView.getMenu().getItem(i);
+            if (items.hasSubMenu()) {
+                unCheckAllMenuItems(items.getSubMenu());
+            } else {
+                items.setChecked(false);
+            }
+        }
+
         drawerLayout.closeDrawer(GravityCompat.START);
         fragmentManager = getSupportFragmentManager();
         fragmentTransaction = fragmentManager.beginTransaction();
@@ -68,24 +80,35 @@ public class NavigationLeft extends AppCompatActivity implements NavigationView.
         switch (item.getItemId()) {
             case R.id.profile:
                 FragmentAccount fragmentAccount = new FragmentAccount();
-                fragmentTransaction.replace(R.id.container,  fragmentAccount);
-                //Toast.makeText(this, "Profile Selected", Toast.LENGTH_SHORT).show();
+                fragmentTransaction.replace(R.id.container, fragmentAccount);
                 break;
             case R.id.contact:
                 FragmentOne fragmentOne = new FragmentOne();
                 fragmentTransaction.replace(R.id.container, fragmentOne);
-                //Toast.makeText(this, "Concat us Selected", Toast.LENGTH_SHORT).show();
                 break;
             case R.id.about:
                 FragmentTwo fragmentTwo = new FragmentTwo();
                 fragmentTransaction.replace(R.id.container, fragmentTwo);
-                //Toast.makeText(this, "About us Selected", Toast.LENGTH_SHORT).show();
                 break;
             case R.id.logout:
                 Toast.makeText(this, "Logout Selected", Toast.LENGTH_SHORT).show();
                 break;
         }
+
+        item.setChecked(true);
         fragmentTransaction.commit();
         return false;
+    }
+
+    private static void unCheckAllMenuItems(Menu menu) {
+        int size = menu.size();
+        for (int i = 0; i < size; i++) {
+            MenuItem item = menu.getItem(i);
+            if(item.hasSubMenu()) {
+                unCheckAllMenuItems(item.getSubMenu());
+            } else {
+                item.setChecked(false);
+            }
+        }
     }
 }
