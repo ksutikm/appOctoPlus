9f72724a92079001011ecc831f539d32063df08e
ksutikm <tsksusha@yandex.ru>
2020-06-03 18:47:36 +0300

    Add new text

--- Workvesion/app/src/main/java/com/example/workvesion/balls/BallsB.java
+++ Workvesion/app/src/main/java/com/example/workvesion/balls/BallsB.java
@@ -3,7 +3,11 @@ package com.example.workvesion.balls;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 import com.example.workvesion.R;
 
@@ -18,6 +22,14 @@ public class BallsB extends AppCompatActivity {
         toolbar = findViewById(R.id.toolbar35);setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setDisplayShowHomeEnabled(true);
+
+        TextView tv_balls_b_1 = findViewById(R.id.tv_balls_b_1);
+        tv_balls_b_1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.uniyar.ac.ru/Abitur/abiturientu-2020/Bac%20i%20spet/%D0%9F%D0%B0%D0%BC%D1%8F%D1%82%D0%BA%D0%B0_2020_%D0%B0%D0%B1%D0%B8%D1%82%D1%83%D1%80%D0%B8%D0%B5%D0%BD%D1%82%D0%B0.pdf")));
+            }
+        });
     }
 
     @Override
--- Workvesion/app/src/main/java/com/example/workvesion/entrance/DocumentsB.java
+++ Workvesion/app/src/main/java/com/example/workvesion/entrance/DocumentsB.java
@@ -1,11 +1,20 @@
 package com.example.workvesion.entrance;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.StrictMode;
+import android.view.View;
+import android.widget.TextView;
 
 import com.example.workvesion.R;
+
+import java.io.File;
 
 public class DocumentsB extends AppCompatActivity {
 
@@ -18,6 +27,39 @@ public class DocumentsB extends AppCompatActivity {
         toolbar = findViewById(R.id.toolbar29);setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setDisplayShowHomeEnabled(true);
+
+        TextView tv_documents_text_b = findViewById(R.id.documents_text_b);
+        tv_documents_text_b.setOnClickListener(new View.OnClickListener() {
+            @RequiresApi(api = Build.VERSION_CODES.JELLY_BEAN_MR2)
+            @Override
+            public void onClick(View v) {
+
+                StrictMode.VmPolicy.Builder builder = new StrictMode.VmPolicy.Builder();
+                StrictMode.setVmPolicy(builder.build());
+                builder.detectFileUriExposure();
+
+                Intent intent = new Intent();
+                intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                intent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
+                intent.setAction(Intent.ACTION_VIEW);
+                String type = "application/msword";
+                intent.setDataAndType(Uri.fromFile(new File("Pamyatka_priem_na_tselevoe_obuchenie_v_ramkakh_kvoty_priema_na_tselevoe_obuchenie.doc")), type);
+                startActivity(intent);
+
+//                Intent intent = new Intent(android.content.Intent.ACTION_VIEW);
+//                File file = new File("Pamyatka_priem_na_tselevoe_obuchenie_v_ramkakh_kvoty_priema_na_tselevoe_obuchenie.doc");
+//                String extension = android.webkit.MimeTypeMap.getFileExtensionFromUrl(Uri.fromFile(file).toString());
+//                String mimetype = android.webkit.MimeTypeMap.getSingleton().getMimeTypeFromExtension(extension);
+//                if (extension.equalsIgnoreCase("") || mimetype == null) {
+//                    // if there is no extension or there is no definite mimetype, still try to open the file
+//                    intent.setDataAndType(Uri.fromFile(file), "text/*");
+//                } else {
+//                    intent.setDataAndType(Uri.fromFile(file), mimetype);
+//                }
+//                // custom message for the intent
+//                startActivity(Intent.createChooser(intent, "Choose an Application:"));
+            }
+        });
     }
 
     @Override
