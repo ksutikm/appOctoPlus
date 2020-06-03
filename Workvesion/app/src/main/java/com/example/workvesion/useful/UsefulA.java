03cc5f1ba62ca1dc027ea689cea56cb43a5d0f2f
ksutikm <tsksusha@yandex.ru>
2020-06-04 00:27:12 +0300

    Add new text

--- Workvesion/app/src/main/java/com/example/workvesion/useful/UsefulA.java
+++ Workvesion/app/src/main/java/com/example/workvesion/useful/UsefulA.java
@@ -3,11 +3,15 @@ package com.example.workvesion.useful;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 import com.example.workvesion.R;
 
-public class UsefulA extends AppCompatActivity {
+public class UsefulA extends AppCompatActivity implements View.OnClickListener {
 
     Toolbar toolbar;
 
@@ -19,11 +23,34 @@ public class UsefulA extends AppCompatActivity {
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setDisplayShowHomeEnabled(true);
+
+        TextView tv_useful_a_1 = findViewById(R.id.tv_useful_a_1);
+        TextView tv_useful_a_2 = findViewById(R.id.tv_useful_a_2);
+        TextView tv_useful_a_3 = findViewById(R.id.tv_useful_a_3);
+
+        tv_useful_a_1.setOnClickListener(this);
+        tv_useful_a_2.setOnClickListener(this);
+        tv_useful_a_3.setOnClickListener(this);
     }
 
     @Override
     public boolean onSupportNavigateUp() {
         onBackPressed();
         return true;
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.tv_useful_a_1:
+                startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.uniyar.ac.ru/Abitur/abiturientu-2020/dni-otkrytykh-dverey-2019-2020.php")));
+                break;
+            case R.id.tv_useful_a_2:
+                startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.youtube.com/user/YarGUTV/videos")));
+                break;
+            case R.id.tv_useful_a_3:
+                startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("https://vk.com/yargutv")));
+                break;
+        }
     }
 }
--- Workvesion/app/src/main/java/com/example/workvesion/useful/UsefulC1.java
+++ Workvesion/app/src/main/java/com/example/workvesion/useful/UsefulC1.java
@@ -3,11 +3,15 @@ package com.example.workvesion.useful;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 import com.example.workvesion.R;
 
-public class UsefulC1 extends AppCompatActivity {
+public class UsefulC1 extends AppCompatActivity implements View.OnClickListener{
 
     Toolbar toolbar;
 
@@ -19,11 +23,34 @@ public class UsefulC1 extends AppCompatActivity {
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setDisplayShowHomeEnabled(true);
+
+        TextView tv_useful_text_c_1_2 = findViewById(R.id.tv_useful_c_1_1);
+        TextView tv_useful_text_c_1_3 = findViewById(R.id.tv_useful_c_1_2);
+        TextView tv_useful_text_c_1_4 = findViewById(R.id.tv_useful_c_1_3);
+
+        tv_useful_text_c_1_2.setOnClickListener(this);
+        tv_useful_text_c_1_3.setOnClickListener(this);
+        tv_useful_text_c_1_4.setOnClickListener(this);
     }
 
     @Override
     public boolean onSupportNavigateUp() {
         onBackPressed();
         return true;
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.tv_useful_c_1_1:
+                startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.uniyar.ac.ru/Abitur/abiturientu-2020/voskresnye-kursy/voskresnyy-lektoriy-fakulteta-biologii-i-ekologii.php")));
+                break;
+            case R.id.tv_useful_c_1_2:
+                startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.uniyar.ac.ru/Abitur/abiturientu-2020/voskresnye-kursy/voskresnyy-lektoriy-po-angliyskomu-yazyku.php")));
+                break;
+            case R.id.tv_useful_c_1_3:
+                startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("http://ivt.uniyar.ac.ru/entrant/preparation")));
+                break;
+        }
     }
 }
