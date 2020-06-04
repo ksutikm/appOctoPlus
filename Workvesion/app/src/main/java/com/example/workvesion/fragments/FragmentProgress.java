8d16ac80359032fd5b9df020e743b36ccfedb0c1
ksutikm <tsksusha@yandex.ru>
2020-06-04 20:47:36 +0300

    Fixes code

--- Workvesion/app/src/main/java/com/example/workvesion/fragments/FragmentProgress.java
+++ Workvesion/app/src/main/java/com/example/workvesion/fragments/FragmentProgress.java
@@ -1,9 +1,11 @@
 package com.example.workvesion.fragments;
 
 import android.os.Bundle;
+import android.text.method.LinkMovementMethod;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -15,6 +17,11 @@ public class FragmentProgress extends Fragment {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_progress, container, false);
+        View view =  inflater.inflate(R.layout.fragment_progress, container, false);
+
+        TextView tv_progress_text = view.findViewById(R.id.text_progress);
+        tv_progress_text.setLinksClickable(true);
+        tv_progress_text.setMovementMethod(LinkMovementMethod.getInstance());
+        return view;
     }
 }
